c4f78de6d1db7a84b9fd8c96e7368c09f9b8566f
ElliottStevanus <EWS38@pitt.edu>
2026-04-13 15:02:20 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,44 +1,38 @@
+// script.js
+
 import { annotateXML } from "./annotator.js";
-import { renderXML } from "./renderer.js";
+import { renderReadingView } from "./renderer.js";
 import { setupUI } from "./ui.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
     const container = document.getElementById("novel-text");
 
+    const xmlText = await fetch("Text/dorian_gray.xml").then(r => r.text());
+    const xml = new DOMParser().parseFromString(xmlText, "text/xml");
+
+    const paragraphs = xml.getElementsByTagName("paragraph");
+
     const patterns = [
         {
-            regex: /\bas\s+[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+){0,2}\s+as\s+[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+){0,2}\b/gi,
+            regex: /\bas\s+.*?\s+as\s+.*?\b/gi,
             tag: "simile"
         },
         {
-            regex: /\blike\s+(?:a|an|the)\s+[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+){0,3}\b/gi,
+            regex: /\blike\s+(a|an|the)\s+.*?\b/gi,
             tag: "simile"
         },
         {
-            regex: /\b(?:is|are|was|were|becomes|became)\s+(?:a|an|the)\s+[a-zA-Z'-]+(?:\s+[a-zA-Z'-]+){0,2}\b/gi,
+            regex: /\b(is|was|were|became)\s+(a|an|the)\s+.*?\b/gi,
             tag: "metaphor"
         }
     ];
 
-    fetch("Text/dorian_gray.xml")
-        .then(res => res.text())
-        .then(xmlText => {
+    const annotatedXML = annotateXML(paragraphs, patterns);
+    const annotatedDoc = new DOMParser().parseFromString(annotatedXML, "text/xml");
 
-            const xml = new DOMParser().parseFromString(xmlText, "text/xml");
-            const paragraphs = xml.getElementsByTagName("paragraph");
+    // default view
+    renderReadingView(annotatedDoc, container);
 
-            // 1. annotate
-            const annotated = annotateXML(paragraphs, patterns);
-
-            // 2. parse XML
-            const annotatedDoc =
-                new DOMParser().parseFromString(annotated, "text/xml");
-
-            // 3. render
-            renderXML(annotatedDoc, container);
-
-            // 4. UI
-            setupUI(container);
-        });
+    setupUI(container, annotatedDoc);
 });
